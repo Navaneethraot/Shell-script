6625d960cc1b43cc280ee46f2a75cb0fd8fc9049
Navaneethraot <youremail@gmail.com>
2024-04-17 12:01:08 +0530

    declaring variables

---
+++ 06-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+MOVIES=("jai" "ABC" "123")
+# size of above array is 3.
+# index are 0,1,2
+
+# list always starts with 0.
+
+echo "First Movie is: ${MOVIES[0]}"
+echo "First Movie is: ${MOVIES[1]}"
+echo "First Movie is: ${MOVIES[@]}"
