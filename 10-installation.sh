1165ae4f89d01da010ad066ef28235ef2d67d886
Navaneethraot <youremail@gmail.com>
2024-04-17 15:01:53 +0530

    declaring variables

---
+++ 10-installation.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+USERID=$(id -u)
+
+if [$USERID -ne 0]
+then 
+    echo "please run this script with root user"
+    exit 1
+ else
+        echo "you are super user"
+ fi
+     dnf install mysql -y
+
+    if [&? -ne 0]
+     then 
+       echo "Installation of mysql...FAILURE"
+       exit 1
+      else
+        echo "Installation of mysql...SUCCESS"
+      fi
+     dnf install git -y
+     then
+        echo "  Installation of mysql...FAILURE"
+        exit 1
+     else
+        echo    "Installation of mysql...SUCCESS"             
+      fi  
