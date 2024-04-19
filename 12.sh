e2977a39a18b5530b2c5fc5692f16925c07df90f
Navaneethraot <youremail@gmail.com>
2024-04-19 19:19:48 +0530

    functions script

--- 12.sh
+++ 12.sh
@@ -6,12 +6,13 @@ SCRIPTNAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
 
 VALIDATE(){
-    if  [ $1 -ne 0]
-        echo "$2...Failure"
-        exit 1
-    else
-        echo "$2 ....Success" 
-    fi       
+    if [ $1 -ne 0 ]
+then
+    echo "$2.....Failure"
+    exit 1
+else
+    echo "$2....Success"
+fi
 }
 
 if  [ $USERID -ne 0]
